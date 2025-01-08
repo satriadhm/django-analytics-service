d146da00fe9a89a4cdf3e4d9b17100753f1ec2ea
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2025-01-08 16:06:01 +0700

    fix: model and migrations

--- analytics/models.py
+++ analytics/models.py
@@ -1,7 +1,8 @@
+import uuid
 from django.db import models
 
 class Analytic(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     source = models.CharField(max_length=255)
     medium = models.CharField(max_length=255)
     campaign = models.CharField(max_length=255)
